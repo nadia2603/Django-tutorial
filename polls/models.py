fa48d50740bdac6f08c6f2b2abc94a35dc839966
nadia2603 <nadia@maykinmedia.nl>
2026-04-02 14:26:19 +0200

    :bulb: - style: voeg comments aan models.py

--- polls/models.py
+++ polls/models.py
@@ -6,8 +6,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+# Charfield voor tesktvelden 
+# Charfield vereist dat je een parameter meegeeft 
+# DateTimeField voor datum-en tijdvelden 
+# question_text en pub_date zijn de namen van het veld 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    # De relatie wordt met behulp van ForeignKey gedefinieerd 
