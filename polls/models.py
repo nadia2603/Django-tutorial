7a9d8ce1540aaf07a6af133a1c4fb51f639ef775
nadia2603 <nadia@maykinmedia.nl>
2026-04-02 14:36:06 +0200

    :sparkles: - feat: registreer polls app in INSTALLED_APPS

--- polls/models.py
+++ polls/models.py
@@ -16,4 +16,4 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    # De relatie wordt met behulp van ForeignKey gedefinieerd 
+# De relatie wordt met behulp van ForeignKey gedefinieerd 
